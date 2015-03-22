0434f12bf0fef195a19ca3d7d7c1a2f8dbe7c237
mperez14 <mperez14@nd.edu>
2015-03-22 17:35:51 -0400

    enemy.h: render skeleton

--- enemy.h
+++ enemy.h
@@ -2,4 +2,14 @@
 //Enemy.h
 
 #include <iostream>
+using namespace std;
 
+class Enemy: public entity{
+	public:
+		Enemy();	//constructor
+		~Enemy();	//deconstructor
+		virtual void render();	//implementation of pure virtual
+	private:
+		stat_type stats;	//creates structure
+		
+};
